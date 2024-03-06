d6c93b6597b7a987235f19391c8a5c477534c1e1
tzuyu2004 <s1113435@o365st.pu.edu.tw>
2024-03-06 17:59:05 +0800

    加入樂透功能

--- app/src/main/java/tw/edu/pu/csim/tzuyu/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tzuyu/lotto/MainActivity.kt
@@ -2,6 +2,7 @@ package tw.edu.pu.csim.tzuyu.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,4 +13,15 @@ class MainActivity : AppCompatActivity() {
         var t: TextView = findViewById(R.id.txv)
         t.text = "0"
     }
+
+
+
+    fun happy(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        if (v.id == R.id.btn){
+            txv.text = (1..100).random().toString()
+        }
+        else{
+            txv.text = "0"
+        }
 }
